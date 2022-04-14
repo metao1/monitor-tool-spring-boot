141b84858342d945fba8ea54643a9a9fcccd6059
mehrdad.karami <mehrdadallahkarami@gmail.com>
2022-04-14 22:43:54 +0200

    completed a test

--- src/test/java/com/metao/monitor/monitortoolspringboot/RemoteHostMonitorTest.java
+++ src/test/java/com/metao/monitor/monitortoolspringboot/RemoteHostMonitorTest.java
@@ -1,6 +1,11 @@
 package com.metao.monitor.monitortoolspringboot;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.metao.monitor.monitortoolspringboot.service.impl.RemoteHostFakeMonitor;
 import com.metao.monitor.monitortoolspringboot.service.impl.UrlValidatorService;
@@ -27,6 +32,12 @@ public class RemoteHostMonitorTest {
     void testMonitor_whenRightRemoteHost_thenReturnReponseData() {
         var url = "https://www.google.com";
         doNothing().when(urlValidatorService).validateUrl(url);
-        fakeMonitor.monitor(url);
+        var responseData = fakeMonitor.monitor(url);
+        verify(urlValidatorService, times(1)).validateUrl(url); // verify that urlValidatorService was called once
+        
+        assertAll("responseData",
+                () -> assertEquals(url, responseData.getUrl()),
+                () -> assertEquals(200, responseData.getStatus()),
+                () -> assertTrue(responseData.getResponseTime() > 0));
     }
 }
